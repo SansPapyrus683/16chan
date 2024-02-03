6979a212f9d2ab57a70f18107c91c4746849a40f
Kevin Sheng <kevinsheng2005@gmail.com>
2024-02-03 08:11:04 -0800

    why can't i find a single good backend

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,18 @@
-import { GeistSans } from "geist/font/sans";
-import "@/styles/globals.css";
-
-const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
+import "@/styles/globals.css"
 
 export const metadata = {
-  metadataBase: new URL(defaultUrl),
   title: "16chan",
-  description: "If Imgur and Danbooru made love.",
-};
+  description: "If Imgur & Danbooru made love",
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
-    <html lang="en" className={GeistSans.className}>
-      <body className="bg-background text-foreground">
-        <main className="min-h-screen flex flex-col items-center">
-          {children}
-        </main>
-      </body>
+    <html lang="en">
+      <body>{children}</body>
     </html>
-  );
+  )
 }
