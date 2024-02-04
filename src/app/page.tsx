6cdb7d5156fd60ba28ac4e4a92c6f83d6fbc124f
Kevin Sheng <kevinsheng2005@gmail.com>
2024-02-04 12:57:48 -0800

    don't you love it when things stop working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,16 @@ import { api } from "@/trpc/react";
 import { toBase64 } from "@/lib/files";
 
 export default function Home() {
-  const [pic, setPic] = useState<File | null>(null);
+  const [pics, setPics] = useState<File[]>([]);
   const uploadPic = api.pic.create.useMutation();
 
   let onSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (pic !== null) {
+    for (let p of pics) {
       uploadPic.mutate({
         post: 1,
-        img: await toBase64(pic),
-        imgType: pic.type,
+        img: await toBase64(p),
+        imgType: p.type,
       });
     }
   };
@@ -25,8 +25,9 @@ export default function Home() {
         <input
           type="file"
           accept="image/*"
+          multiple
           onChange={(e) => {
-            setPic(e.target.files![0]!);
+            setPics([...pics, ...Array.from(e.target.files!)]);
           }}
         />
         <button type="submit">submit</button>
