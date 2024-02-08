60f53ee365d17624bf274ff7f17e9d4528b81e5d
Kevin Sheng <kevinsheng2005@gmail.com>
2024-02-08 15:24:27 -0800

    new db schema and url paths

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,8 +4,16 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ["/", "/new", "/api/webhooks(.*)"],
+  publicRoutes: [
+    "/",
+    "/new",
+    "/search",
+    "/album",
+    "/post/((?!create).*(?<!/edit)$)",
+    "/api/webhooks(.*)",
+  ],
 });
+
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/(api|trpc)(.*)"],
 };
