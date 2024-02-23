852301f51186d9f1b25595bdd823b81ac1a7e734
Michael Zhou <micha.zhou@gmail.com>
2024-02-23 15:19:47 -0800

    smal style change

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -7,26 +7,18 @@ export function PageLayout() {
     <nav className="flex fixed w-full h-16 justify-between items-center bg-gray-200 p-6 pl-12 pr-12 text-xl">
       <div className="flex space-x-16">
         <div className="">
-          <Link href="/">
-            16chan
-          </Link>
+          <Link href="/">16chan</Link>
         </div>
         <div className="">
-          <Link href="/following">
-            Following
-          </Link>
+          <Link href="/following">Following</Link>
         </div>
         <div className="">
-          <Link href="/post/create">
-            New Post
-          </Link>
+          <Link href="/post/create">New Post</Link>
         </div>
       </div>
       <div className="flex space-x-16">
         <div className="">
-          <Link href="/account">
-            Account
-          </Link>
+          <Link href="/account">Account</Link>
         </div>
       </div>
     </nav>
