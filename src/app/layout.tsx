2b21ac8918ffc0a3e29441fafc9135847bb60291
Michael Zhou <micha.zhou@gmail.com>
2024-02-23 14:20:51 -0800

    separated PageLayout into its own component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "@/globals.css";
-import Link from "next/link"
 
 import { Button } from "@/components/ui/button"
+import { PageLayout } from "@/components/PageLayout.tsx"
 
 import { Comic_Neue } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
@@ -29,45 +29,7 @@ export default function RootLayout({
       <body className={comicNeue.className}>
         <TRPCReactProvider>
           <ClerkProvider>
-            <nav className="bg-gray-200 flex p-6 pl-12 pr-12 justify-between text-xl">
-              <div className="flex space-x-16">
-                <div className="">
-                  <Link href="/">
-                    16chan
-                  </Link>
-                </div>
-                <div className="">
-                  <Link href="/following">
-                    Following
-                  </Link>
-                </div>
-                <div className="">
-                  <Link href="/post/create">
-                    New Post
-                  </Link>
-                </div>
-              </div>
-              <div className="flex space-x-16">
-                <div className="">
-                  <Link href="/account">
-                    Account
-                  </Link>
-                </div>
-              </div>
-            </nav>
-              {/*
-              <div className="bg-green-400 flex items-center justify-between space-y-2">
-                <h2 className="bg-green-500 text-3xl font-bold tracking-tight">Djshboard</h2>
-                <h2 className="bg-green-500 text-3xl font-bold tracking-tight">Ajasdf</h2>
-                <h2 className="bg-green-500 text-3xl font-bold tracking-tight">Asdfasd asdf</h2>
-                <h2 className="bg-green-500 text-3xl font-bold tracking-tight">Asdfasd asdf</h2>
-                <div className="bg-green-500 flex items-center space-x-2">
-                  <div>adsf</div>
-                  <div>adsf</div>
-                  <div>adsf</div>
-                </div>
-              </div>
-  */}
+            <PageLayout/>
             {children}
           </ClerkProvider>
         </TRPCReactProvider>
