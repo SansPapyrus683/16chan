7be8aa018da5459aa74d9ae44eee85c799c20326
Michael Zhou <micha.zhou@gmail.com>
2024-02-23 14:46:24 -0800

    fixed rebase

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "@/globals.css";
-import Link from "next/link";
 import React from "react";
+
+import { PageLayout } from "@/components/PageLayout.tsx"
 
 import { Comic_Neue } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
@@ -24,24 +25,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className={comicNeue.className}>
         <TRPCReactProvider>
           <ClerkProvider>
-            <nav className="flex justify-between bg-gray-200 p-6 pl-12 pr-12 text-xl">
-              <div className="flex space-x-16">
-                <div className="">
-                  <Link href="/">16chan</Link>
-                </div>
-                <div className="">
-                  <Link href="/following">Following</Link>
-                </div>
-                <div className="">
-                  <Link href="/post/create">New Post</Link>
-                </div>
-              </div>
-              <div className="flex space-x-16">
-                <div className="">
-                  <Link href="/account">Account</Link>
-                </div>
-              </div>
-            </nav>
+            <PageLayout/>
             {children}
           </ClerkProvider>
         </TRPCReactProvider>
