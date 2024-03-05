54fc0263b64d7000625f5f9d79471abf47e850a0
Michael Zhou <micha.zhou@gmail.com>
2024-03-04 20:07:44 -0800

    progress on page.tsx for individual post display

--- src/app/account/page.tsx
+++
@@ -1,7 +0,0 @@
-import { notFound, redirect } from "next/navigation";
-import { currentUser } from "@clerk/nextjs";
-
-export default async function accountRedirect() {
-  const user = await currentUser();
-  return user ? redirect(`/account/${user.username}`) : notFound();
-}
