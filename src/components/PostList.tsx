d88270feb1b67c5fa276bf9d1eb18a5d2d475020
Aiden Wong <aidenpwong@gmail.com>
2024-03-05 18:13:23 -0800

    Added automatic resizing. There are still some errors where it cannot access the image, but I will fix those later

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -10,6 +10,7 @@ import Link from "next/link";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { LikeButton } from "@/components/LikeButton";
 import { useAuth } from "@clerk/nextjs";
+import { PhoneNumber } from "@clerk/clerk-sdk-node";
 
 type PostData = RouterOutputs["user"]["userPosts"];
 
@@ -108,104 +109,124 @@ export function PostList({
 }) {
   // hm you'd think this would be undefined for a lil @ the start but it actually isn't
   const { userId } = useAuth();
+  const [photoRows, setPhotoRows] = useState<{
+    [num: number]: { startIndex?: number; endIndex?: number; height?: number };
+  }>({});
   const [photoDimensions, setPhotoDimensions] = useState<{
     [id: string]: { width?: number; height?: number };
   }>({});
 
   useEffect(() => {
     const fetchPhotoDimensions = async () => {
-      let count: number = 0;
-      const dimensions: { [id: string]: { width?: number; height?: number } } = {};
-      const track_photos: {
-        [num: number]: { id?: string; width?: number; height?: number };
+      const dimensions: {
+        [id: string]: { width?: number; height?: number };
+      } = {};
+      const retPhotoDimensions: {
+        [id: string]: { width?: number; height?: number };
+      } = {};
+      const retPhotoRows: {
+        [num: number]: { startIndex?: number; endIndex?: number };
       } = {};
       await Promise.all(
         posts.map(async (photo) => {
-          try {
-            const img = document.createElement("img");
-            img.src = photo.images[0]!.img;
-            await img.decode();
-            dimensions[photo.id] = { width: img.width, height: img.height };
-            track_photos[count] = {
-              id: photo.id,
-              width: img.width,
-              height: img.height,
-            };
-            count = count + 1;
-          } catch (error) {
-            console.error(`Error loading image: ${photo.id}`, error);
+          let tries = 5;
+          let completed = false;
+          while (tries > 0 && !completed) {
+            try {
+              const img = document.createElement("img");
+              img.src = photo.images[0]!.img;
+              await img.decode();
+              dimensions[photo.id] = {
+                width: img.width,
+                height: img.height,
+              };
+              completed = true;
+            } catch (error) {
+              console.log(error);
+              tries--;
+            }
           }
+          if (!completed) console.log(`failed to load image ${photo.id}`);
         }),
       );
 
       const MAX_WIDTH = window.innerWidth;
+      const MAX_HEIGHT = 500;
+
       let currWidthPixelCount: number = 0;
       let currHeightPixelCount: number = 0;
       let backLog: number = 0;
-
-      for (let i = 0; i < Object.keys(dimensions).length; i++) {
-        const width = track_photos[i]?.width;
-        const height = track_photos[i]?.height;
+      let rowCount: number = 0;
+      let start: number = 0;
+      let imagesInRow = [];
+      Object.keys(dimensions).map((id) => {
+        let width = dimensions[id]?.width;
+        let height = dimensions[id]?.height;
+        let scale_factor = 0;
         if (width != undefined && height != undefined) {
-          currWidthPixelCount += width;
-          currHeightPixelCount += height;
+          scale_factor = MAX_HEIGHT / height;
+          currWidthPixelCount += width * scale_factor;
+          currHeightPixelCount += height * scale_factor;
           backLog++;
+          imagesInRow.push(id);
         }
-        if (currWidthPixelCount >= MAX_WIDTH) {
-          for (let j = 0; j < backLog; j++) {
-            let chosenHeight = currHeightPixelCount / backLog;
-            const id = track_photos[i - j]?.id;
-            if (id !== undefined && dimensions[id] !== undefined) {
-              const oldDimensions = dimensions[id];
-              if (
-                oldDimensions?.height !== undefined &&
-                oldDimensions?.width !== undefined
-              ) {
-                const scaleFactor = oldDimensions.height / chosenHeight;
-                dimensions[id] = {
-                  ...oldDimensions,
-                  height: oldDimensions.height * scaleFactor,
-                  width: oldDimensions.width * scaleFactor,
-                };
-                console.log(`resized ${id} to ${scaleFactor}`);
-              }
-            }
-          }
+        if (currWidthPixelCount >= MAX_WIDTH && backLog >= 5) {
+          let new_height: number =
+            (MAX_WIDTH / currWidthPixelCount) * (height * scale_factor);
+          imagesInRow.forEach((imageID) => {
+            retPhotoDimensions[imageID] = {
+              height: new_height,
+              width:
+                dimensions[imageID]?.width *
+                (MAX_HEIGHT / dimensions[imageID]?.height) *
+                (MAX_WIDTH / currWidthPixelCount),
+            };
+          }),
+            (retPhotoRows[rowCount] = {
+              startIndex: start,
+              endIndex: start + backLog,
+            });
           currWidthPixelCount = 0;
           currHeightPixelCount = 0;
+          imagesInRow = [];
+          start = start + backLog;
           backLog = 0;
+          rowCount++;
         }
-      }
-
-      setPhotoDimensions(dimensions);
+      }),
+        setPhotoRows(retPhotoRows);
+      setPhotoDimensions(retPhotoDimensions);
     };
     fetchPhotoDimensions();
   }, [posts]);
 
   return (
     <div>
-      {Object.keys(photoDimensions).length > 0 ? (
-        <div className="grid grid-cols-3 gap-4">
-          {posts.map((p) => (
-            <div key={p.id} className="flex flex-col">
-              <Link href={`/post/${p.id}`}>
-                <Image
-                  key={p.id}
-                  className="w-auto"
-                  src={p.images[0]!.img}
-                  alt="post preview"
-                  width={photoDimensions[p.id]?.width || 300}
-                  height={photoDimensions[p.id]?.height || 300}
-                />
-              </Link>
-              <Link href={`/post/${p.id}`}>{p.title}</Link>
-              {p.id}
-              {likeButton && (
-                <LikeButton
-                  pid={p.id}
-                  liked={p.likes!.some((i) => i.userId === userId)}
-                />
-              )}
+      {Object.keys(photoRows).length > 0 && Object.keys(photoDimensions).length > 0 ? (
+        <div className="grid">
+          {Object.keys(photoRows).map((index) => (
+            <div className="flex">
+              {posts
+                .slice(photoRows[index].startIndex, photoRows[index].endIndex)
+                .map((p) => (
+                  <div key={p.id} className="flex flex-col">
+                    <Link href={`/post/${p.id}`}>
+                      <Image
+                        key={p.id}
+                        src={p.images[0]!.img}
+                        alt="post preview"
+                        width={photoDimensions[p.id]?.width || 300}
+                        height={photoDimensions[p.id]?.height | 300}
+                      />
+                    </Link>
+                    {likeButton && (
+                      <LikeButton
+                        pid={p.id}
+                        liked={p.likes!.some((i) => i.userId === userId)}
+                      />
+                    )}
+                  </div>
+                ))}
             </div>
           ))}
         </div>
