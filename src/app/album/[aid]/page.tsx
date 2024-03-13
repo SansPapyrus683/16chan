1f38b4e9200eb34c9c6a2f4cd82f9387de5919ab
Kevin Sheng <kevinsheng2005@gmail.com>
2024-03-13 12:15:06 -0700

    conditionally render delete album

--- src/app/album/[aid]/page.tsx
+++ src/app/album/[aid]/page.tsx
@@ -2,17 +2,23 @@ import { api } from "@/trpc/server";
 import { DeleteAlbum } from "@/components/DeleteAlbum";
 import { serialize, serverFetch } from "@/lib/utils";
 import { Album } from "@/components/Album";
+import { auth } from "@clerk/nextjs/server";
 
-export default async function AlbumView({ params }: { params: { aid: string } }) {
-  const ret = await serverFetch(async () => await api.album.get(params.aid));
+export default async function AlbumView({
+  params: { aid },
+}: {
+  params: { aid: string };
+}) {
+  const ret = await serverFetch(async () => await api.album.get(aid));
   if (!ret.good) {
     return <div>{ret.err}</div>;
   }
+  const { userId } = auth();
 
   return (
     <div className="space-y-4">
-      <Album aid={params.aid} initAlbum={serialize(ret.val)} />
-      <DeleteAlbum aid={params.aid} />
+      <Album aid={aid} initAlbum={serialize(ret.val)} />
+      {userId == ret.val!.userId && <DeleteAlbum aid={aid} />}
     </div>
   );
 }
